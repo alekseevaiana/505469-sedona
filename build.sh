e69245090c882f1079511af02ae609f5b7f75f05
alekseevayana <alekseevayana@bk.ru>
2018-04-04 22:12:53 +0200

    Финальная проверка, попытка 3

---
+++ build.sh
@@ -0,0 +1,3 @@
+postcss css/style.css --no-map --use autoprefixer --output css/style.css
+postcss css/style.css --no-map --use autoprefixer cssnano --output css/style.min.css
+uglifyjs js/script.js --compress --mangle --output js/script.min.js
